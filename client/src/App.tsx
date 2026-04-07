8d1c9ac83f24854ecb1d800d77453d6f7e7ed540
PawanPawar11 <pawanpawar4369@gmail.com>
2026-04-07 23:23:46 +0530

    feat: Initial frontend setup

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="bg-zinc-800 min-h-screen text-white flex items-center justify-center text-3xl">
+      App
+    </div>
+  );
+};
+
+export default App;
